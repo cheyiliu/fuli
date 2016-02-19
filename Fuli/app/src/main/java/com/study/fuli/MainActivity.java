3558fa1beb0dac276edf21c364c66739d3b0be99
hsy <houshengyong@diditaxi.com.cn>
2016-02-19 18:21:23 +0800

    add ui stub

--- Fuli/app/src/main/java/com/study/fuli/MainActivity.java
+++ Fuli/app/src/main/java/com/study/fuli/MainActivity.java
@@ -1,13 +1,53 @@
 package com.study.fuli;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private PagerSlidingTabStrip mTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager.setAdapter(new MainViewPagerAdapter());
+        mTab = (PagerSlidingTabStrip) findViewById(R.id.tab);
+        mTab.setViewPager(mViewPager);
+        mTab.setIndicatorColor(R.color.colorPrimary);
+        mTab.setIndicatorHeight((int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 3, getResources().getDisplayMetrics()));
+    }
+
+    private class MainViewPagerAdapter extends FragmentPagerAdapter {
+        private String[] mTitles = getResources().getStringArray(R.array.tab_titles);
+
+        public MainViewPagerAdapter() {
+            super(getSupportFragmentManager());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new PageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return mTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
     }
 }
---
+++ Fuli/app/src/main/java/com/study/fuli/PageFragment.java
@@ -0,0 +1,31 @@
+package com.study.fuli;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hsy on 2016/2/19.
+ */
+public class PageFragment extends Fragment {
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_fl, container, false);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) root.findViewById(R.id.swiperefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("test", "onRefresh");
+            }
+        });
+        return root;
+    }
+}
